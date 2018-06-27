7cf5d556c46eb9604f7c2e834464e1eac62b9f65
Alan Zhao <alanzhaonys@gmail.com>
2018-06-27 10:25:52 -0400

    require root for certbot.sh

--- certbot.sh
+++ certbot.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Make sure only root can run this script
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root (sudo)" 1>&2
+    exit 1
+fi
+
 if [[ $# -eq 0 ]] ; then
   echo Enter a domain name
   exit
